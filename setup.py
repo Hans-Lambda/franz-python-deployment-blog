ae24b6cf54487eded3562e6999ca8f60491da174
Franz Bandelin <bifiroll86@gmail.com>
2022-06-08 15:41:04 +0200

    fourth commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    version="0.4",
+    version="0.6",
     name="franz-blog",
     author="Franz Bandelin, copied from Mathias Brito",
     author_email="franz@whatever.com",
-    description="This is the blog system created by Mathias and then crippled by Franz",
+    description="This is the blog system created by Mathias and then crippled by Franz - changed description to see if token works",
     long_description="I still hate Testpypy naming conventions!",
     url="https://github.com/Hans-Lambda/franz-python-deployment-blog",
     license="MIT",
@@ -13,6 +13,6 @@ setup(
     requires=[],
     packages=find_packages(),
     entry_points={
-        "console_scripts": ['blog=blog.main:main']
+        "console_scripts": ['funny=blog.main:main']
     }
 )
